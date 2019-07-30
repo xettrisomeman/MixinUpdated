1867e3f18c501e09e98fb20de564266969c87e14
webizfabulous <webizmoxx@gmail.com>
2019-07-30 12:44:54 +0545

    Aliite definations added

--- djangomixin/mixin.py
+++ djangomixin/mixin.py
@@ -2,10 +2,10 @@ from django.core.exceptions import PermissionDenied
 
 
 class OneRequired:
-
+        #when class is fired , dispatch is the first method it calls
     def dispatch(self, request,*args,**kwargs):
-        if kwargs['id'] != 1:
-            raise PermissionDenied
-
+        if kwargs['id'] != 1: #if id provided in url is not equal to 1 then
+            raise PermissionDenied #raise permissiondenied
+        #else
         return super(OneRequired ,self).dispatch(request,*args,**kwargs)
 
